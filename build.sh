e3347a6a380e980b9f66e4410d840b4fde392738
Ilham Ramadhan <ilham@jkt1.ebdesk.com>
2021-01-27 14:52:47 +0700

    adding git ignore

--- build.sh
+++
@@ -1,4 +0,0 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o api/oauth2-test
-docker build -t 192.168.247.22/ima/oauth2-test .
-docker rmi $(docker images -f "dangling=true" -q) -f || true
-docker push 192.168.247.22/ima/oauth2-test
